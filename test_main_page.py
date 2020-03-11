38e5def07a7d911993d986f38efa5a46282c4301
Konstantin Ivanov <seyros@mail.ru>
2020-03-11 11:53:12 +0300

    Initialase login_page in test body

--- test_main_page.py
+++ test_main_page.py
@@ -10,28 +10,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
-def test_guest_should_be_login_url(browser):
-    page = LoginPage(browser, login_link)
-    page.open()
-    page.should_be_login_url()
-
-
-def test_guest_should_see_login_form(browser):
-    page = LoginPage(browser, login_link)
-    page.open()
-    page.should_be_login_form()
-
-
-def test_guest_should_see_register_form(browser):
-    page = LoginPage(browser, login_link)
-    page.open()
-    page.should_be_register_form()
-
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
